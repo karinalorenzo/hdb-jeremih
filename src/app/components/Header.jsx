eebf4d9ebb92a0acc96071c9be3fd63233662fc8
karinalorenzo <karinalrg16@gmail.com>
2025-10-17 12:15:27 +0200

    fix styles to the Header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Header = () => {
   return (
-    <header className="fixed top-6 w-full text-white p-4 z-50">
+    <header className="fixed  w-full text-white p-4 z-50">
       <div className="flex items-center">
         <a
           href="/"
